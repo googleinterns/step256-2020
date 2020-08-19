0b5a101720e60491e0b003650529a5846b7a90b2
Raluca Tudor <tudorr30@gmail.com>
2020-08-19 06:33:49 +0000

    Fix indentation

--- src/main/java/com/google/sps/servlets/ShoppingResultsServlet.java
+++ src/main/java/com/google/sps/servlets/ShoppingResultsServlet.java
@@ -17,18 +17,18 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/search-shopping-results")
 public class ShoppingResultsServlet extends HttpServlet {
-
+  
   public static final String GOOGLE_SEARCH_URL = 
       "https://www.google.com//search?safe=strict&hl=en&tbm=shop&source=h&tbs=vw:l";  // &tbs=vw:g for view and removes ads, tbm=shop means google shopping
   
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
-		String searchURL = GOOGLE_SEARCH_URL + "&q=Running%20Shoes%20for%20women&num=10";
-
-		// Without proper User-Agent, we will get 403 error
-		Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
-
-	response.setContentType("text/html");
+    
+    String searchURL = GOOGLE_SEARCH_URL + "&q=Running%20Shoes%20for%20women&num=10";
+    
+    // Without proper User-Agent, we will get 403 error
+    Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+    
+    response.setContentType("text/html");
     response.getWriter().println(doc.html());
   }
 }
