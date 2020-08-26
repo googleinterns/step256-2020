58c2994d3e584e36f3f14860a296141a0c152626
Raluca Tudor <tudorr30@gmail.com>
2020-08-26 16:52:31 +0000

    Add PhotoShoppingServlet

---
+++ src/main/java/com/google/sps/servlets/PhotoShoppingServlet.java
@@ -0,0 +1,49 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.sps.data.GoogleShoppingResultsWrapper;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/photo-shopping-request")
+public class PhotoShoppingServlet extends HttpServlet {
+  
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // TODO: Based on request.getParameter("photo-category"), call methods from photo detection classes, 
+    // passing request.getParameter("blob-key") as argument. These methods build the shopping query and call 
+    // the {@code getShoppingResultsPage} method from GoogleShoppingResultsWrapper.
+
+    response.setContentType("text/html");
+
+    switch (request.getParameter("photo-category")) {
+      case "product":
+        response.getWriter().println(GoogleShoppingResultsWrapper.getShoppingResultsPage("Fountain pen"));
+        break;
+      case "list":
+        Sresponse.getWriter().println(GoogleShoppingResultsWrapper.getShoppingResultsPage("Fuzzy socks"));
+        break;
+      case "barcode":
+        response.getWriter().println(GoogleShoppingResultsWrapper.getShoppingResultsPage("Running shoes"));
+        break;
+      default:
+        response.getWriter().println(GoogleShoppingResultsWrapper.getShoppingResultsPage("Notebook"));
+    }
+  }
+}
