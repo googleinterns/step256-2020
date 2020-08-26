3a7f99f8be19c485bfea545a37de4d9d54c99a68
saakshisaxena <saakshi.saxena2000@gmail.com>
2020-08-26 17:37:29 +0000

    Update comment

--- src/main/java/com/google/sps/data/GoogleShoppingResultsWrapper.java
+++ src/main/java/com/google/sps/data/GoogleShoppingResultsWrapper.java
@@ -19,8 +19,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 /**
- * Class responsible for returning the Google Shopping results for a given query 
- * in the form of an HTML page. 
+ * Return the Google Shopping results for a given query in the form of an HTML page. 
  */
 public class GoogleShoppingResultsWrapper {
 
