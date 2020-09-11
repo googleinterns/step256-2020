9958016e99a830adc038f210da893c76f4bc62e2
Raluca <tudorr30@gmail.com>
2020-09-11 16:05:20 +0300

    Update ProductListExtractor tests

--- src/test/java/com/google/sps/ProductListExtractorTest.java
+++ src/test/java/com/google/sps/ProductListExtractorTest.java
@@ -41,18 +41,23 @@ public final class ProductListExtractorTest {
   }
 
   @Test
-  public void CompleteDoc() throws Exception {
+  public void CompleteProductData() throws Exception {
     List<Product> expectedProducts = new ArrayList<>();
     expectedProducts.add(Product.create("TWSBI Eco <b>Fountain Pen</b> - Clear - Extra-Fine", 
         "https://image-link-1.com", 
         "<span class=\"HRLxBb\">$30.99</span> from Goldspot", 
         "https://product-link-1.com", 
         "+$4.95 shipping"));
+    expectedProducts.add(Product.create("Faber-Castell Fountain Pen", 
+        "https://image-link-2.com", 
+        "<span class=\"HRLxBb\">$20.00</span> from Faber-Castell", 
+        "https://product-link-2.com", 
+        "Free shipping"));
     
-    File completeDocFile = new File("./src/main/webapp/fake-shopping-results/complete-doc.html");
-    Document completeDoc = Jsoup.parse(completeDocFile, "UTF-8");
+    Document mockResultsDoc = 
+        Jsoup.parse(new File("./src/main/webapp/mock-shopping-results/complete-data.html"), "UTF-8");
 
-    List<Product> actualProducts = productListExtractor.extract(completeDoc);
+    List<Product> actualProducts = productListExtractor.extract(mockResultsDoc);
 
     Assert.assertEquals(expectedProducts, actualProducts);
   }
@@ -66,10 +71,23 @@ public final class ProductListExtractorTest {
         "https://product-link-1.com", 
         "+$4.95 shipping"));
     
-    File completeDocFile = new File("./src/main/webapp/fake-shopping-results/product-rating-missing.html");
-    Document completeDoc = Jsoup.parse(completeDocFile, "UTF-8");
+    Document mockResultsDoc = 
+        Jsoup.parse(
+            new File("./src/main/webapp/mock-shopping-results/product-rating-missing.html"), "UTF-8");
 
-    List<Product> actualProducts = productListExtractor.extract(completeDoc);
+    List<Product> actualProducts = productListExtractor.extract(mockResultsDoc);
+
+    Assert.assertEquals(expectedProducts, actualProducts);
+  }
+
+  @Test
+  public void NoProductData() throws Exception  {
+    List<Product> expectedProducts = new ArrayList<>();
+    
+    Document mockResultsDoc = 
+        Jsoup.parse(new File("./src/main/webapp/mock-shopping-results/no-data.html"), "UTF-8");
+
+    List<Product> actualProducts = productListExtractor.extract(mockResultsDoc);
 
     Assert.assertEquals(expectedProducts, actualProducts);
   }
